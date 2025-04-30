bd9e27f9f02468730c271c467cedeece47ee31b8
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-05-01 00:06:57 +0600

    generated type defs

---
+++ rollup.config.mjs
@@ -0,0 +1,11 @@
+import dts from 'rollup-plugin-dts';
+import { defineConfig } from 'rollup';
+
+export default defineConfig({
+	input: './src/index.ts',
+	output: {
+		file: './dist/index.d.ts',
+		format: 'es',
+	},
+	plugins: [dts()],
+});
